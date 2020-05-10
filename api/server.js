c5c48434cf594e4388c3c8af4d39460c3f27b5c2
Sarah-Angel <sarangel333@gmail.com>
2020-05-10 13:37:15 +0300

    Node express server skeleton

---
+++ api/server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+import config from '../config/config'
+
+const app = express()
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    console.log("root")
+    res.send("root")
+})
+
+mongoose.Promise = global.Promise //use ES6 promises
+mongoose.connect(config.mongoUri)
+mongoose.connection.on('error', () => {
+    throw new Error('Unable to connect to database.')
+})
+
+app.listen(config.port, (err) => {
+    if (err) {
+        console.error(err)
+    }
+
+    console.info("Server started on port: " + config.port)
+})
